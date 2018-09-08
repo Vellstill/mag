67a22968e49b1fcd4a59ef2c0289a6b8f679691b
Vellstill <flash.89@list.ru>
2018-09-08 19:22:53 +0300

    header & footer

---
+++ footer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="footer">
+        <div class = "footer-info">
+            <h4>КОЛЛЕКЦИИ</h4>
+            <a href="#">Женщинам</a><br>
+            <a href="#">Мужчинам</a><br>
+            <a href="#">Детям</a><br>
+            <a href="#">Новинки</a>
+        </div>
+        <div class="separ"></div>
+        <div class = "footer-info center">
+            <h4>МАГАЗИН</h4>
+            <a href="#">О нас</a><br>
+            <a href="#">Доставка</a><br>
+            <a href="#">Работай с нами</a><br>
+            <a href="#">Контакты</a>
+        </div>
+        <div class="separ"></div>
+        <div class = "footer-info right">
+            <h4>МЫ В СОЦИАЛЬНЫХ СЕТЯХ</h4>
+            <p>
+                Сайт разработан в inordic.ru <br>
+                2018 © Все права защищены.
+                <div class = "social">
+                    <a href="#">
+                        <img src="/images/twitter.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="/images/facebook.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="/images/instagram.png" alt="">
+                    </a>
+                </div>
+            </p>
+        </div>
+    </div>
+</body>
+</html>
