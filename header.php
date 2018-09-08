0f27cc8e52663ec786ac744220e6888c03707a46
Vellstill <flash.89@list.ru>
2018-09-08 16:43:26 +0300

    header для страниц

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class = "header">
+        <nav class = "navigation">
+            <a href="" class="logo-pic"></a>
+            <div class = "nav"><a href="#">Женщинам</a></div>
+            <div class = "nav"><a href="#">Мужчинам</a></div>
+            <div class = "nav"><a href="#">Детям</a></div>
+            <div class = "nav"><a href="#">Новинки</a></div>
+            <div class = "nav"><a href="#">О нас</a></div>
+        </nav>
+        <div class="client">
+            <div class = "client-login">
+                <a href="#" class = "login">Войти</a>
+            </div>
+            <div class = "client-bascet">
+                <a href="#" class="bascet">Корзина</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
