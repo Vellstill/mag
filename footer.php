94fda3e9fda2f9c75577d2047d96b41a5bb1ceaa
Vellstill <flash.89@list.ru>
2018-09-09 20:30:32 +0300

    1 лист

--- footer.php
+++ footer.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
+
     <div class="footer">
         <div class = "footer-info">
             <h4>КОЛЛЕКЦИИ</h4>
@@ -44,5 +35,7 @@
             </p>
         </div>
     </div>
+    <!-- bootstrap JavaScript -->  
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php 
+include "header.php"
+?>
+<div class="top">
+    <h1>НОВЫЕ ПОСТУПЛЕНИЯ ВЕСНЫ</h1>
+    <h4>Мы подготовили для Вас лучшие новинки сезона</h4>
+    <div class="new-btn">
+        <a href="">ПОСМОТРЕТЬ НОВИНКИ</a>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-3 first"></div>
+        <div class="col-lg-3 second">
+            <div class="attention"></div>
+            <p>
+                каждый сезон мы подготавливаем для Вас исключительно лучшую модную одежду. Следит за нашими новинками
+            </p>
+        </div>
+        <div class="col-lg-3 third"></div>
+        <div class="col-lg-3 fourth">
+
+        </div>
+        <div class="col-lg-3 fifth"></div>
+        <div class="col-lg-3 sixth">
+            <div class="attention"></div>
+            <p>
+                Самые низкие цены в Москве.Нашли дешевле? Вернем разницу.
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 eighth"></div>
+        <div class="col-lg-3 ninth">
+
+        </div>
+        <div class="col-lg-3 tenth"></div>
+        <div class="col-lg-3 seventh"></div>
+    </div>
+</div>
+<div class="bottom">
+    <h2>БУДЬ ВСЕГДА В КУРСЕ ВЫГОДНЫХ ПРЕДЛОЖЕНИЙ</h2>
+    <h4>подписывайся и следи за новинками и выгодными предложениями.</h4>
+    <form action="">
+        <input type="email" class = "email-input" name = "email" placeholder = "e-mail">
+        <button type="submit" class = "submit-btn">записаться</button>
+    </form>
+</div>
+
+
+
+<?php 
+include "footer.php"
+?>
+
+
+
